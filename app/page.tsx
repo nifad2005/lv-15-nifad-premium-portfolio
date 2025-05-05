f7c326cde3d255e3bce40d7c052ca6d4a28d1a19
Md Nifad Uzzaman <nifaduzzaman2005@gmail.com>
2025-05-05 23:05:24 +0600

    Started the website

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <main>
-      <h1 className="text-6xl">Nifad Premium</h1>
-   </main>
+    <>
+    <main className="min-h-screen bg-radial from-slate-500 blur-[0.5px]   to-90%  from-10% min-w-screen grid place-items-center  to-slate-900">
+        <h1 className="text-6xl text-transparent bg-gradient-to-r from-sky-400   to-slate-900 bg-clip-text font-bold">Nifad Premium</h1>
+    </main>
+    </>
   );
 }
